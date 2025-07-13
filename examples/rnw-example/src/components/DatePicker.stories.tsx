e82bc87056a88d6c1e4c71d68ee1bf8d99541feb
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-07-13 19:17:19 +0100

    fix: it

--- examples/rnw-example/src/components/DatePicker.stories.tsx
+++ examples/rnw-example/src/components/DatePicker.stories.tsx
@@ -1,8 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { DatePicker } from "./datepicker";
+import { Text, View } from "react-native";
 
 const meta: Meta<typeof DatePicker> = {
   component: DatePicker,
+  decorators: [
+    (Story) => (
+      <View className="items-start">
+        <Text>
+          DatePicker, if this doesn't error then flow type removal is working
+        </Text>
+        <Story />
+      </View>
+    ),
+  ],
 };
 
 export default meta;
